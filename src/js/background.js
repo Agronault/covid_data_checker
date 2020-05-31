78365a4de9b135cd0e1a2635fc9dbc4ba6bc52de
IagorSs <iagor.penaranda@uol.com.br>
2020-05-30 22:51:49 -0300

    NOW ??

--- src/js/background.js
+++ src/js/background.js
@@ -1,26 +1,30 @@
-var parags = $('p')
-
-parags.each(verifyParagraph);
-
-if(haveCOVID) check();
-else chrome.browserAction.setIcon({path:"../../icons/icon.png"})
-
 chrome.browserAction.onClicked.addListener(change);
+chrome.webNavigation.onCompleted.addListener(checkCOVID);
 
-var haveCOVID = false;
 var siteSafe = true;
+var hasCOVID = false;
 
-function check(){
-	if (siteSafe) chrome.browserAction.setIcon({path:"../../icons/not.png"});
-	else chrome.browserAction.setIcon({path:"../../icons/ok.png"});
+function checkCOVID(){
+	var parags = $('p');
+
+	parags.each(verifyParagraph(hasCOVID));
+
+	if(hasCOVID) checkSite();
+	else chrome.browserAction.setIcon({path:"../../icons/icon.png"})
+}
+
+function checkSite(){
+	if (siteSafe) chrome.browserAction.setIcon({path:"../../icons/ok.png"});
+	else chrome.browserAction.setIcon({path:"../../icons/not.png"});
 }
 
 function verifyParagraph(i, parag) {
-    const paragText = $(parag).text()
-    if (/COVID/.test(paragText)) haveCOVID = true;
+    const paragText = $(parag).text();
+    if (/COVID/.test(paragText)) hasCOVID = true;
 }
 
 function change(){
 	if (siteSafe) siteSafe = false;
 	else siteSafe = true;
+	checkSite();
 }
