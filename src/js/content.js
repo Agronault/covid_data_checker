f6b22c40668b58ff745f9cdb57f12e38eacb7dd4
Agronault <xande.luis@hotmail.com>
2020-05-30 21:20:28 -0300

    highlights coronavirus content

--- src/js/content.js
+++ src/js/content.js
@@ -1,13 +1,10 @@
 var parags = $('p')
-//chrome.browserAction.setIcon({path:'src/not.png'})
 
 parags.each(verifyParagraph)
 
 function verifyParagraph(i, parag) {
     const paragText = $(parag).text()
     if (/coronavirus/.test(paragText)) {
-        alert(paragText)
-        
-        //chrome.browserAction.setIcon({path:'src/not.png'})
+        parag.innerHTML = `<span style = "background: rgba(0, 255, 87, 0.57);" >${paragText}</span>`
     }
 }
