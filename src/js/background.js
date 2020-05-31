1f3bd9a812e72330d5c39cdd6f73c3b6e7601d64
IagorSs <iagor.penaranda@uol.com.br>
2020-05-31 18:24:34 -0300

    check site implementted

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,12 @@
-let summary = {}
+const urlJSON = chrome.runtime.getURL("src/data/untrustedUrls.JSON");
+
+fetch(urlJSON)
+  .then((response) => response.json())
+  .then(function(myJSON){
+    summary = myJSON;
+  })
+
+var summary;
 
 chrome.runtime.onConnect.addListener((portFrom) => {
     if(portFrom.name === 'background-content') {
@@ -7,12 +15,9 @@ chrome.runtime.onConnect.addListener((portFrom) => {
               const hasCovid = message.payload.hasCovid
               if (hasCovid) {
                 chrome.browserAction.setIcon({path: {"19": "/icons/ok_19.png", "38": "/icons/ok_38.png"}, tabId: sendingPort.sender.tab.id})
-                fetch('https://api.covid19api.com/summary').then(r => r.text()).then(result => {
-                    if (!/You have reached maximum request limit./i.test(result)) {
-                        summary = JSON.parse(result)
-                    }
-                })
-                alert(message.payload.url)
+                var completeURL = message.payload.url;
+                var finalURL = adjustURL(completeURL,sendingPort);
+                checkURL(finalURL,sendingPort);
               } else {
                 chrome.browserAction.setIcon({path: {"19": "/icons/idle_icon_19.png", "38": "/icons/idle_icon_38.png"}, tabId: sendingPort.sender.tab.id})
               }
@@ -27,4 +32,19 @@ function fetchData () {
             summary = JSON.parse(result)
         }
     })
+}
+
+function adjustURL(completeURL){
+  var begin = completeURL.indexOf('w.') + 2;
+  var partialURL = completeURL.slice(begin);
+  var final = partialURL.indexOf('/');
+  return partialURL.slice(0, final);
+}
+
+function checkURL(URL,sendingPort){
+  for(var i = 0; i < summary.untrusted.length ; i++)
+    if(!summary.untrusted[i].toUpperCase().localeCompare(URL.toUpperCase())){
+      chrome.browserAction.setIcon({path: {"19": "/icons/not_19.png", "38": "/icons/not_38.png"}, tabId: sendingPort.sender.tab.id});
+      break;
+    }
 }
